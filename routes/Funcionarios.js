ac232c504cb4539a50c23f1ed954a82366d83f5b
João Pedro <joao.cintra7@hotmail.com>
2024-06-09 19:33:38 -0300

    att
    att

--- routes/Funcionarios.js
+++ routes/Funcionarios.js
@@ -2,70 +2,6 @@ const express = require('express');
 const Funcionario = require('../models/Funcionario');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  try {
-    const funcionarios = await Funcionario.find();
-    res.json(funcionarios);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const funcionario = await Funcionario.findById(req.params.id);
-    if (!funcionario)
-      return res.status(404).json({ message: 'Funcionario não encontrado' });
-    res.json(funcionario);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-router.post('/', async (req, res) => {
-  const funcionario = new Funcionario({
-    Nome: req.body.Nome,
-    Cargo: req.body.Cargo,
-    Departamento: req.body.Departamento,
-    Salario: req.body.Salario,
-  });
-  try {
-    const newFuncionario = await funcionario.save();
-    res.status(201).json(newFuncionario);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedFuncionario = await Funcionario.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
-    if (!updatedFuncionario)
-      return res.status(404).json({ message: 'Funcionario não encontrado' });
-    res.json(updatedFuncionario);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedFuncionario = await Funcionario.findByIdAndDelete(req.params.id);
-    if (!deletedFuncionario)
-      return res.status(404).json({ message: 'Funcionario não encontrado' });
-    res.json({ message: 'Funcionario deletado' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
 /**
  * @swagger
  * /api/funcionarios:
@@ -173,6 +109,72 @@ router.delete('/:id', async (req, res) => {
  *       '500':
  *         description: Erro interno do servidor.
  */
+
+
+router.get('/', async (req, res) => {
+  try {
+    const funcionarios = await Funcionario.find();
+    res.json(funcionarios);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const funcionario = await Funcionario.findById(req.params.id);
+    if (!funcionario)
+      return res.status(404).json({ message: 'Funcionario não encontrado' });
+    res.json(funcionario);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  const funcionario = new Funcionario({
+    Nome: req.body.Nome,
+    Cargo: req.body.Cargo,
+    Departamento: req.body.Departamento,
+    Salario: req.body.Salario,
+  });
+  try {
+    const newFuncionario = await funcionario.save();
+    res.status(201).json(newFuncionario);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedFuncionario = await Funcionario.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+    if (!updatedFuncionario)
+      return res.status(404).json({ message: 'Funcionario não encontrado' });
+    res.json(updatedFuncionario);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedFuncionario = await Funcionario.findByIdAndDelete(req.params.id);
+    if (!deletedFuncionario)
+      return res.status(404).json({ message: 'Funcionario não encontrado' });
+    res.json({ message: 'Funcionario deletado' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,42 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { swaggerUi, swaggerDocs } = require('./swagger');
 
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const mongoURI = 'mongodb+srv://zegitajubile0:CPU8lNwdQwQNt9Kc@trabalho.h7railx.mongodb.net/?retryWrites=true&w=majority&appName=Trabalho';
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Funcionario:
+ *       type: object
+ *       properties:
+ *         Nome:
+ *           type: string
+ *           description: Nome do funcionário.
+ *         Cargo:
+ *           type: string
+ *           description: Cargo do funcionário.
+ *         Departamento:
+ *           type: string
+ *           description: Departamento do funcionário.
+ *         Salario:
+ *           type: number
+ *           description: Salário do funcionário.
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Funcionários
+ *   description: Endpoints para gerenciamento de funcionários
+ */
+
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -25,3 +56,5 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
